4d8a3594cca182234d92ecf7c16ae2fc796c43fb
Leblayd <leblaydavid7@gmail.com>
2018-06-08 01:28:33 +0200

    remastered admin/add route to use Setter methods instead of Constructors (also made the code a lot more logical, extracted methods to Utils.java)

--- src/main/java/com/codecool/library/controller/admin/Add.java
+++ src/main/java/com/codecool/library/controller/admin/Add.java
@@ -1,8 +1,8 @@
 package com.codecool.library.controller.admin;
 
+import com.codecool.library.Utils;
 import com.codecool.library.controller.DBConnect;
 import com.codecool.library.model.BaseModel;
-import org.reflections.Reflections;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,82 +11,72 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns = {"/admin/add"})
 public class Add extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.sendRedirect("/");
-        Map<String, String> parameters = getParameters(req.getParameterMap());
-
-        addModelToDB(req, parameters);
-    }
-
-    private void addModelToDB(HttpServletRequest req, Map<String, String> parameters) {
-        Class<? extends BaseModel> classType = getModelClass(req.getParameter("type"));
+        Class<? extends BaseModel> classType = Utils.getModelClass(req.getParameter("type"));
         if (classType == null) return;
 
-        BaseModel newObject = null;
-        List<Object> paramValues = new ArrayList<>();
-        ArrayList<Class> paramClasses = new ArrayList<>();
         try {
-            for (Map.Entry<String, String> entry : parameters.entrySet()) {
-                Field field = classType.getDeclaredField(entry.getKey());
-                Class<?> type = field.getType();
-                paramClasses.add(type);
+            Map<String, String> stringMap = Utils.getParameters(req.getParameterMap());
+            Map<String, Object> parameterMap = getParameters(stringMap, classType);
+            BaseModel newObject = getNewObject(classType, parameterMap);
 
-                if (type == String.class) {
-                    paramValues.add(entry.getValue());
-                } else if (type.equals(int.class) || (type.equals(Integer.class))) {
-                    paramValues.add(Integer.valueOf(entry.getValue()));
-                } else if (type.equals(Boolean.class)) {
-                    paramValues.add(Boolean.valueOf(entry.getValue()));
-                } else {
-                    // TODO
-                    System.out.println("INCOMPLETE!!!");
+            DBConnect.persist(newObject);
+        } catch (NoSuchFieldException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        resp.sendRedirect("/");
+    }
+
+    private Map<String, Object> getParameters(Map<String, String> stringMap, Class<? extends BaseModel> classType)
+            throws NoSuchFieldException, IllegalAccessException, InvocationTargetException {
+
+        Map<String, Object> parameterMap = new HashMap<>();
+        for (Map.Entry<String, String> entry : stringMap.entrySet()) {
+            Field field = classType.getDeclaredField(entry.getKey());
+            Class<?> type = field.getType();
+
+            // TODO implement the valueOf method in all extending classes of BaseModel!!!
+            // TODO and also, there HAVE TO BE setters for every single field of every class
+            Object param;
+            if (type.getClass().getName().equals("java.lang.String")) {
+                param = entry.getValue();
+            } else {
+                try {
+                    param = type.getMethod("valueOf", String.class).invoke(null, entry.getValue());
+                } catch (NoSuchMethodException e) {
+                    e.printStackTrace();
+                    continue;
                 }
             }
+            parameterMap.put(field.getName(), param);
+        }
+        return parameterMap;
+    }
 
-            Class[] paramClassArray = new Class[paramClasses.size()];
-            paramClasses.toArray(paramClassArray);
-
-            Object[] paramValueArray = new Object[paramValues.size()];
-            paramValues.toArray(paramValueArray);
-
-            newObject = classType
-                    .getConstructor(paramClassArray)
-                    .newInstance(paramValueArray);
-
+    private BaseModel getNewObject(Class<? extends BaseModel> classType, Map<String, Object> parameterMap) {
+        BaseModel baseModel = null;
+        try {
+            baseModel = classType.getConstructor().newInstance();
+            // Call each setter method for each passed parameter
+            for (Map.Entry<String, Object> entry : parameterMap.entrySet()) {
+                String setterName = "set" +
+                        entry.getKey().substring(0, 1).toUpperCase() + entry.getKey().substring(1);
+                Method setter = baseModel.getClass().getMethod(setterName, entry.getValue().getClass());
+                setter.invoke(baseModel, entry.getValue());
+            }
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             e.printStackTrace();
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
         }
-        DBConnect.persist(newObject);
+        return baseModel;
     }
 
-    private Map<String, String> getParameters(Map<String, String[]> map) {
-        return map.entrySet().stream()
-                .filter(set -> set.getValue() != null && !set.getKey().equalsIgnoreCase("type"))
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey, t -> t.getValue()[0])
-                );
-    }
-
-    private Class<? extends BaseModel> getModelClass(String className) {
-        Reflections reflections = new Reflections("com.codecool.library");
-        Set<Class<? extends BaseModel>> allClasses = reflections.getSubTypesOf(BaseModel.class);
-        for (Class<? extends BaseModel> modelClass : allClasses) {
-            if (modelClass.getSimpleName().equalsIgnoreCase(className)) {
-                return modelClass;
-            }
-        }
-        return null;
-    }
 }
