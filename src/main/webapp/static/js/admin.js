67499b85d6bbe6eaa155bc3d4e38424eb4333cd0
Leblayd <leblaydavid7@gmail.com>
2018-06-20 14:51:03 +0200

    refactored to properly use JQuery

--- src/main/webapp/static/js/admin.js
+++ src/main/webapp/static/js/admin.js
@@ -1,23 +1,17 @@
-$.each($(".model-button"), function (index, object) {
-    var button = $(object);
-    object.addEventListener("click", function() {
-        $.ajax(button.data("url"), {
-            success: function (data) {
-                $("#content").html($.param(data._embedded));
-            }
+function addSidebarEventListeners() {
+    $.each($(".model-button"), function (index, object) {
+        var button = $(object);
+        button.click(function () {
+            $.ajax(button.data("url"), {
+                success: function (data) {
+                    $("#content").html($.param(data._embedded));
+                }
+            });
         });
     });
-});
-// for (let i = 0; i < modelButtons.length; i++) {
-//     let button = $(modelButtons[i]);
-//     modelButtons[i].addEventListener("click", function() {
-//         console.log(button);
-//
-//         $.ajax(button.data("url"), {
-//             success: function (data) {
-//                 console.log(data._embedded.books);
-//                 $("#content").html($.param(data._embedded));
-//             }
-//         });
-//     });
-// }
+}
+
+$(document).ready(
+    addSidebarEventListeners()
+
+);
