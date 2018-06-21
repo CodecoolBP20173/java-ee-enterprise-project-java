4f4e0cc36d8cd93c308ad173b5f12e4fd1c824ea
Balázs Vajner <vajner.balazs@gmail.com>
2018-06-21 11:03:44 +0200

    Integrated search results into main page

--- src/main/webapp/static/js/layout.js
+++ src/main/webapp/static/js/layout.js
@@ -1,14 +1,22 @@
 function search() {
-    $.each($("#selectpicker option:selected"), function (index, object) {
-        var button = $(object);
+    $(".search-result-container").hide();
 
-        $.ajax(button.data("url"), {
-            success: function (data) {
-                console.log(data);
-            }
+    $.each($("#selectpicker option:selected"), function (index, object) {
+        let button = $(object);
+        let searchResultTargetSelector = button.data("target");
+        let searchResultTarget = $(searchResultTargetSelector);
+        let searchResultTargetContainer = $(`.search-result-container ${searchResultTargetSelector}`).parent("div");
+        searchResultTargetContainer.show();
+        searchResultTarget.text("Loading result, please wait...");
+
+        $.get(button.data("url"), {"q":$("#search-term").val()},  function (data) {
+            searchResultTarget.html(data);
+        }).fail(function(xhr){
+            searchResultTarget.html(`<span class="alert alert-danger"> Error ${xhr.status}, please contact support.</span>`);
         });
     });
 }
 $(function () {
     $("#search").click(search);
+    $(".search-result-container").hide();
 });
