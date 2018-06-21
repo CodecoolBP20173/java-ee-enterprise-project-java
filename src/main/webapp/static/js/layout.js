35e15ef62b32b4be89dcd84d86bc6a4e61cbbab1
Balázs Vajner <vajner.balazs@gmail.com>
2018-06-21 14:24:18 +0200

    Added working pagination

--- src/main/webapp/static/js/layout.js
+++ src/main/webapp/static/js/layout.js
@@ -1,19 +1,37 @@
+function reloadResult(url, target, query, size, page){
+    let searchResultTarget = $(target);
+    let searchResultTargetContainer = $(`.search-result-container ${target}`).parent("div");
+    searchResultTargetContainer.show();
+    //searchResultTarget.text("Loading result, please wait...");
+
+    $.get(url, {"q":query, "size": size, "page": page},  function (data) {
+        searchResultTarget.html(data);
+        searchResultTarget.find(".page-item").click(function(event){
+            reloadResult(url, target, query, size, $(event.target).parent().data("page"));
+        });
+    }).fail(function(xhr){
+        searchResultTarget.html(`<span class="alert alert-danger"> Error ${xhr.status}, please contact support.</span>`);
+    });
+}
+
+
 function search() {
     $(".search-result-container").hide();
 
     $.each($("#selectpicker option:selected"), function (index, object) {
-        let button = $(object);
-        let searchResultTargetSelector = button.data("target");
+        let option = $(object);
+        reloadResult(option.data("url"), option.data("target"), $("#search-term").val(), $("#max-results").val(), 0);
+        /*let searchResultTargetSelector = option.data("target");
         let searchResultTarget = $(searchResultTargetSelector);
         let searchResultTargetContainer = $(`.search-result-container ${searchResultTargetSelector}`).parent("div");
         searchResultTargetContainer.show();
         searchResultTarget.text("Loading result, please wait...");
 
-        $.get(button.data("url"), {"q":$("#search-term").val()},  function (data) {
+        $.get(option.data("url"), {"q":$("#search-term").val(), "size": $("#max-results").val()},  function (data) {
             searchResultTarget.html(data);
         }).fail(function(xhr){
             searchResultTarget.html(`<span class="alert alert-danger"> Error ${xhr.status}, please contact support.</span>`);
-        });
+        });*/
     });
 }
 $(function () {
