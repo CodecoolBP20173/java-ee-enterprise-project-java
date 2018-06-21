1aa549508682b1650064b18046b3545795a6b3ba
Leblayd <leblaydavid7@gmail.com>
2018-06-21 11:51:58 +0200

    authors can now be added, and their data loads

--- src/main/webapp/static/js/admin.js
+++ src/main/webapp/static/js/admin.js
@@ -2,20 +2,57 @@ function addSidebarEventListeners() {
     $.each($(".model-button"), function (index, object) {
         var button = $(object);
         button.click(function () {
-            $.ajax(button.data("url"), {
-                success: function (data) {
-                    $("#content").html(data);
-                }
-            });
+            loadTable(button);
         });
     });
 }
 
 function addObjectEventListener() {
+    let button = $("#add-row div#button");
+    button.click(function () {
+        var formData = {};
+        $("#add-row").find("input[name]").each(function (index, node) {
+            if ($(node).attr("type") === "checkbox") {
+                formData[node.name] = !!$(node).attr("checked");
+            } else {
+                formData[node.name] = node.value;
+            }
+        });
 
+        $.ajax(button.data("url"), {
+            method: "POST",
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(formData),
+            success: function () {
+                loadTable()
+            }
+        })
+    })
+}
+
+var lastLoaded;
+
+function loadTable(button) {
+    if (!button) {
+        button = lastLoaded;
+    } else {
+        lastLoaded = button;
+    }
+    $.ajax(button.data("url"), {
+        success: function (data) {
+            $("#content").html(data);
+            refresh();
+        }
+    });
+}
+
+function refresh() {
+
+    addObjectEventListener();
 }
 
 $(document).ready(
-    addSidebarEventListeners()
-
+    addSidebarEventListeners(),
+    refresh()
 );
