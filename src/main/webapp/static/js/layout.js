b25e96104ccb3484c8399a8e3448404622766d1b
Balázs Vajner <vajner.balazs@gmail.com>
2018-06-22 09:04:30 +0200

    No longer "jumping" when clicking the paginator

--- src/main/webapp/static/js/layout.js
+++ src/main/webapp/static/js/layout.js
@@ -7,6 +7,8 @@ function reloadResult(url, target, query, size, page){
     $.get(url, {"q":query, "size": size, "page": page},  function (data) {
         searchResultTarget.html(data);
         searchResultTarget.find(".page-item:not(.disabled)").click(function(event){
+            event.preventDefault();
+            event.stopPropagation();
             reloadResult(url, target, query, size, $(event.target).parent().data("page"));
         });
     }).fail(function(xhr){
