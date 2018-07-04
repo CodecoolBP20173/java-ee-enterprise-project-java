fb8466142eea57ffdea39c202b7c350bbeb6a092
Balázs Vajner <vajner.balazs@gmail.com>
2018-07-04 21:51:07 +0200

    Password changing now works

--- src/main/java/com/codecool/library/controller/admin/AdminApi.java
+++ src/main/java/com/codecool/library/controller/admin/AdminApi.java
@@ -2,16 +2,12 @@ package com.codecool.library.controller.admin;
 
 import com.codecool.library.model.Admin;
 import com.codecool.library.repository.AdminRepository;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.Optional;
@@ -21,24 +17,68 @@ import java.util.Optional;
 public class AdminApi {
 
     @Autowired
+    public AdminApi(AdminRepository adminRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.adminRepository = adminRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    static class PasswordChangeData {
+        @JsonProperty("old-password")
+        String oldPassword;
+        @JsonProperty("new-password")
+        String newPassword;
+        @JsonProperty("new-password-again")
+        String newPasswordAgain;
+
+        public PasswordChangeData() {
+        }
+
+        boolean repeatedCorrectly() {
+            return newPassword.equals(newPasswordAgain);
+        }
+
+        boolean sameAsOld(){
+            return newPassword.equals(oldPassword);
+        }
+
+        String getOldPassword() {
+            return oldPassword;
+        }
+
+        String getNewPassword() {
+            return newPassword;
+        }
+
+    }
+
+    private final
     AdminRepository adminRepository;
 
-    @Autowired
+    private final
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @PostMapping("/change-password")
-    ResponseEntity<String> updatePassword(Principal principal,
-                        @RequestParam("old-password") String oldPassword,
-                        @RequestParam("new-password") String newPassword,
-                        @RequestParam("new-password-again") String newPasswordAgain) {
-        Optional<Admin> admin = adminRepository.findByUsername(principal.getName());
+    ResponseEntity<String> updatePassword(Principal principal, @RequestBody PasswordChangeData passwordChangeData) {
+        Optional<Admin> adminOptional = adminRepository.findByUsername(principal.getName());
 
-        if(!admin.isPresent())
-            return new ResponseEntity<String>("User not logged in", HttpStatus.UNAUTHORIZED);
+        if(!adminOptional.isPresent())
+            return new ResponseEntity<>("User not logged in", HttpStatus.UNAUTHORIZED);
 
-        if(!newPassword.equals(newPasswordAgain))
-            return new ResponseEntity<String>("New password repeated incorrectly.", HttpStatus.BAD_REQUEST);
+        Admin admin = adminOptional.get();
 
-        return new ResponseEntity<String>("", HttpStatus.OK);
+        if(!passwordChangeData.repeatedCorrectly())
+            return new ResponseEntity<>("New password repeated incorrectly.", HttpStatus.BAD_REQUEST);
+
+        if(passwordChangeData.sameAsOld())
+            return new ResponseEntity<>("New password is identical to the old one.", HttpStatus.BAD_REQUEST);
+
+        if(!bCryptPasswordEncoder.matches(passwordChangeData.getOldPassword(), admin.getPassword()))
+            return new ResponseEntity<>("Incorrect password entered.", HttpStatus.UNAUTHORIZED);
+
+        admin.setPassword(bCryptPasswordEncoder.encode(passwordChangeData.getNewPassword()));
+
+        adminRepository.save(admin);
+
+        return new ResponseEntity<>("", HttpStatus.OK);
     }
 }
