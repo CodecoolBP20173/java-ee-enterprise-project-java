2c9adab2a18754d2bb32684c8cc85bcbb27a8c5e
László Noémi <mynadesign@gmail.com>
2018-07-04 15:11:01 +0200

    Authors are added to database through initializer

--- src/main/java/com/codecool/library/service/InitializerBean.java
+++ src/main/java/com/codecool/library/service/InitializerBean.java
@@ -9,9 +9,11 @@ import com.codecool.library.repository.AuthorRepository;
 import com.codecool.library.repository.BookRepository;
 import com.codecool.library.repository.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class InitializerBean {
@@ -23,15 +25,45 @@ public class InitializerBean {
             adminRepository.save(admin);
         }
 
-        Author author1 = new Author("László", "Bock");
-        authorRepository.save(author1);
+        List<Author> authors = new ArrayList<>();
+        authors.add(new Author("László", "Bock"));
+        authors.add(new Author("Sue", "Hadfield"));
+        authors.add(new Author("Gill", "Hasson"));
+        authors.add(new Author("Josh", "Seiden"));
+        authors.add(new Author("Jeff", "Gothelf"));
+        authors.add(new Author("Chris", "McChesney"));
+        authors.add(new Author("Sean", "Covey"));
+        authors.add(new Author("Jim", "Huling"));
+        authors.add(new Author("Ádám", "Nagy"));
+        authors.add(new Author("Ágnes", "Antal"));
+        authors.add(new Author("Mónika", "Holzer"));
+        authors.add(new Author("Daniel H", "Pink"));
+        authors.add(new Author("Herbert", "Schildt"));
+        authors.add(new Author("Patric", "Lencioni"));
+        authors.add(new Author("Carrie Anne", "Phillhin"));
+        authors.add(new Author("Robert", "C. Martin"));
+        authors.add(new Author("Max", "Kanat-Alexander"));
+        authors.add(new Author("Domonkos", "Kotsis"));
+        authors.add(new Author("Gábor", "Légrádi"));
+        authors.add(new Author("Gergely", "Nagy"));
+        authors.add(new Author("Sándor", "Szénási"));
+        authors.add(new Author("Andrew", "Hunt"));
+        authors.add(new Author("David", "Thomas"));
+        authors.add(new Author("István", "Reiter"));
+        authors.add(new Author("Carol S.", "Dweck"));
+        authors.add(new Author("Josh", "Carter"));
+        authors.add(new Author("József", "Kovács"));
+        authors.add(new Author("Gábor", "Takács"));
+        authors.add(new Author("Miklós", "Takács"));
+        authors.add(new Author("Frederic", "Laloux"));
+        authorRepository.saveAll(authors);
 
         Publisher publisher1 = new Publisher("Bookline");
         publisherRepository.save(publisher1);
-        
+
 //        Author author, String title, Publisher publisher, String location, Integer publicationYear
         bookRepository.save(
-                new Book(author1,"A google-titok", publisher1, "Szekszárd", 2015)
+                new Book(authors.get(0),"A google-titok", publisher1, "Szekszárd", 2015)
         );
 
     }
